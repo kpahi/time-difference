6b18928e330a7dc7550cd6e05b11a9ca4e23325f
Kritish Pahi <kritish.pahi@ranenetwork.com>
2018-04-04 18:00:50 +0545

    Data via POST

--- app/routes.py
+++ app/routes.py
@@ -18,11 +18,15 @@ def index():
     return render_template('index.html', title='Kritish', user=user, posts=posts) 
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+@app.route('/compute', methods=['GET', 'POST'])
+def compute():
+    
+    form_data = request.form
+    all_start_date = form_data.getlist('start')
+    all_end_date = form.data.getlist('end')
+    # import ipdb; ipdb.set_trace()
+    # form = LoginForm()
+    # if form.validate_on_submit():
+    #     print(form)
+    #     return redirect(url_for('compute'))
+    return render_template('thanks.html')
